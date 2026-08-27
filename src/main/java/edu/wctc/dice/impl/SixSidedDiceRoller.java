47341dfe9681caf68c8a8425247e12d58ddb7f6e
Andrew Harris <aharris36@my.wctc.edu>
2026-08-26 21:44:41 -0500

    component tags

--- src/main/java/edu/wctc/dice/impl/SixSidedDiceRoller.java
+++ src/main/java/edu/wctc/dice/impl/SixSidedDiceRoller.java
@@ -1,8 +1,11 @@
 package edu.wctc.dice.impl;
 
 import edu.wctc.dice.iface.DiceRoller;
+import org.springframework.stereotype.Component;
+
 import java.util.Random;
 
+@Component
 public class SixSidedDiceRoller implements DiceRoller {
 
     @Override
--- src/main/java/edu/wctc/dice/impl/TwelveSidedDiceRoller.java
+++ src/main/java/edu/wctc/dice/impl/TwelveSidedDiceRoller.java
@@ -1,9 +1,11 @@
 package edu.wctc.dice.impl;
 
 import edu.wctc.dice.iface.DiceRoller;
+import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Component
 public class TwelveSidedDiceRoller implements DiceRoller {
 
     @Override
